580cc2fe96696df2243353e2720e7b0bd5fc9277
tytestelle <ccszdk@gmail.com>
2026-09-02 21:28:58 +0800

    Update group_list.dart

--- lib/widgets/group_list.dart
+++ lib/widgets/group_list.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:coocaa_flutter_focus/coocaa_flutter_focus.dart';
 
 class GroupList extends StatelessWidget {
   final List<String> groups;
@@ -7,48 +6,31 @@ class GroupList extends StatelessWidget {
   final ValueChanged<String> onSelect;
 
   const GroupList({
-    super.key,
+    Key? key,
     required this.groups,
     this.selectedGroup,
     required this.onSelect,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black.withOpacity(0.2),
-      child: ListView.builder(
-        itemCount: groups.length,
-        itemBuilder: (context, index) {
-          final group = groups[index];
-          final isSelected = group == selectedGroup;
-          return FocusableWidget(
-            focusId: 'group_$index',
-            onTap: () => onSelect(group),
-            focusDecoration: FocusDecoration(
-              boxDecoration: BoxDecoration(
-                color: Colors.blue.withOpacity(0.25),
-                borderRadius: BorderRadius.circular(4),
-                border: Border.all(color: Colors.blue, width: 2),
-              ),
+    return ListView.builder(
+      itemCount: groups.length,
+      itemBuilder: (context, index) {
+        final group = groups[index];
+        final isSelected = group == selectedGroup;
+        return ListTile(
+          title: Text(
+            group,
+            style: TextStyle(
+              color: isSelected ? Colors.yellow : Colors.white70,
+              fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+              fontSize: 14,
             ),
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
-              decoration: BoxDecoration(
-                color: isSelected ? Colors.blue.withOpacity(0.15) : Colors.transparent,
-              ),
-              child: Text(
-                group,
-                style: TextStyle(
-                  color: isSelected ? Colors.yellow : Colors.white70,
-                  fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
-                  fontSize: 14,
-                ),
-              ),
-            ),
-          );
-        },
-      ),
+          ),
+          onTap: () => onSelect(group),
+        );
+      },
     );
   }
 }
